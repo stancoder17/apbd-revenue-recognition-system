540d2646282f2b5be52b1164075478493d537521
s31552 <s31552@pjwstk.edu.pl>
2025-08-29 10:50:15 +0200

    Configure database connection settings

---
+++ Revenue recognition system/Data/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Revenue_recognition_system.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Revenue_recognition_system.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) {}
+
+    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Client> Clients { get; set; }
+    public DbSet<IndividualClient> IndividualClients { get; set; }
+    public DbSet<CompanyClient> CompanyClients { get; set; }
+    public DbSet<Contract> Contracts { get; set; }
+    public DbSet<SupportExtension> SupportExtensions { get; set; }
+    public DbSet<Payment> Payments { get; set; }
+    public DbSet<PaymentMethod> PaymentMethods { get; set; }
+    public DbSet<PaymentTarget> PaymentTargets { get; set; }
+    public DbSet<Discount> Discounts { get; set; }
+    public DbSet<ClientDiscount> ClientDiscounts { get; set; }
+    public DbSet<SoftwareVersionDiscount> SoftwareVersionDiscounts { get; set; }
+    public DbSet<Software> Softwares { get; set; }
+    public DbSet<SoftwareVersion> SoftwareVersions { get; set; }
+    public DbSet<Status> Statuses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
--- Revenue recognition system/Program.cs
+++ Revenue recognition system/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using Revenue_recognition_system.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
